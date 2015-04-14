2c37468a3cd38133b16b988602cd2773b29d9998
fugor1103 <fugor1103@gmail.com>
2015-04-14 18:38:52 +0800

    11

--- src/PointOfSaleTest.java
+++ src/PointOfSaleTest.java
@@ -1,9 +1,4 @@
-import static org.junit.Assert.*;
 import junit.framework.TestCase;
-
-import org.junit.Before;
-import org.junit.Test;
-
 import Model.ConcreteIceCream;
 import Model.ConcreteIceCreamDecorator;
 
